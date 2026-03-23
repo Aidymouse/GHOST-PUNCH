3ec71c22c31a342924a2c51577715ba6c48f46cd
Aidan <aidymouse4891@gmail.com>
2026-03-23 22:29:31 +1300

    untested wave orb particle spawning

--- Assets/Scripts/WaveOrb.cs
+++ Assets/Scripts/WaveOrb.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class WaveOrb : MonoBehaviour
 {
     public float expansion_speed;
+    public ParticleSystem wave_particles;
+    public Vector3 particle_offset;
     public float life_timer;
     Timer life;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
       life = new Timer(life_timer);
+
+      Instantiate(wave_particles, transform.position + particle_offset);
     }
 
     // Update is called once per frame
