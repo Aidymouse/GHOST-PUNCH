032892dcb8e494ac0a8f2a542822daaf83e6e9a3
Aidan <aidymouse4891@gmail.com>
2026-04-04 00:43:08 +1300

    WaveOrb code format

--- Assets/Scripts/WaveOrb.cs
+++ Assets/Scripts/WaveOrb.cs
@@ -2,31 +2,31 @@ using UnityEngine;
 
 public class WaveOrb : MonoBehaviour
 {
-    public float expansion_speed;
-    public ParticleSystem wave_particles;
-    public Vector3 particle_offset;
-    public float life_timer;
-    Timer life;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-      life = new Timer(life_timer);
+	public float expansion_speed;
+	public ParticleSystem wave_particles;
+	public Vector3 particle_offset;
+	public float life_timer;
+	Timer life;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		life = new Timer(life_timer);
 
-      Instantiate(wave_particles, transform.position + particle_offset, transform.rotation);
-    }
+		Instantiate(wave_particles, transform.position + particle_offset, transform.rotation);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-      float expansion = expansion_speed * Time.deltaTime;
+		float expansion = expansion_speed * Time.deltaTime;
 
-      transform.localScale += new Vector3(expansion, expansion, expansion);
+		transform.localScale += new Vector3(expansion, expansion, expansion);
 
-      life.tick(Time.deltaTime);
-      if (life.finished()) {
-	Destroy(this.gameObject);
-      }
-        
-    }
+		life.tick(Time.deltaTime);
+		if (life.finished()) {
+			Destroy(this.gameObject);
+		}
+
+	}
 }
