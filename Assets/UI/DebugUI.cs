4c7113954e0813c6bb27932cec06c7f01ebf746a
Aidan <aidymouse4891@gmail.com>
2026-04-08 23:30:28 +1200

    refining ragdolling of the ghost

--- Assets/UI/DebugUI.cs
+++ Assets/UI/DebugUI.cs
@@ -3,71 +3,81 @@ using TMPro;
 
 public class DebugUI : MonoBehaviour
 {
-		public Ghost ghost;
-		public GhostPuncher ghost_puncher;
+  string[] ghost_action_strings = {
+    "Charging Escape",
+    "Startled",
+    "Moving Room" ,
+    "Hit Stun" ,
+    "Ragdoll",
+    "Recovery",
+    "Using Power"
+  };
 
-		TMP_Text ui_debug1; // Ghost action
-		TMP_Text ui_debug2; // Ghost hitstun timer
-		TMP_Text ui_debug3; 
+  public Ghost ghost;
+  public GhostPuncher ghost_puncher;
 
-		TMP_Text ghost_state;
-		TMP_Text ghost_power;
-		TMP_Text ghost_power_timer;
+  TMP_Text ui_debug1; // Ghost action
+  TMP_Text ui_debug2; // Ghost hitstun timer
+  TMP_Text ui_debug3; 
 
-		void Start()
-		{
-				TMP_Text[] texts = GetComponentsInChildren<TMP_Text>();
+  TMP_Text ghost_state;
+  TMP_Text ghost_power;
+  TMP_Text ghost_power_timer;
 
-				foreach (TMP_Text text in texts) {
-						switch (text.name) {
-								case "Debug1": 
-										ui_debug1 = text;
-										break;
+  void Start()
+  {
+    TMP_Text[] texts = GetComponentsInChildren<TMP_Text>();
 
-								case "Debug2": 
-										ui_debug2 = text;
-										break;
+    foreach (TMP_Text text in texts) {
+      switch (text.name) {
+	case "Debug1": 
+	  ui_debug1 = text;
+	  break;
 
-								case "Debug3": 
-										ui_debug3 = text;
-										break;
+	case "Debug2": 
+	  ui_debug2 = text;
+	  break;
+
+	case "Debug3": 
+	  ui_debug3 = text;
+	  break;
 
 
-								case "DebugGhostState": 
-										ghost_state = text;
-										break;
+	case "DebugGhostState": 
+	  ghost_state = text;
+	  break;
 
-								case "DebugGhostPower": 
-										ghost_power = text;
-										break;
+	case "DebugGhostPower": 
+	  ghost_power = text;
+	  break;
 
-								case "DebugGhostPowerTimer": 
-										ghost_power_timer = text;
-										break;
+	case "DebugGhostPowerTimer": 
+	  ghost_power_timer = text;
+	  break;
 
-						}
-				}
-		}
+      }
+    }
+  }
 
-		void Update() {
-			//SetDebug1("Ghost Poise: "+ghost.poise);
-		}
+  void Update() {
+    SetDebug1("Ghost Action: "+ghost_action_strings[(int)ghost.cur_action]);
+  }
 
-		public void SetDebug1(string text){
-				ui_debug1.SetText(text);
-		}
+  public void SetDebug1(string text){
+    ui_debug1.SetText(text);
+  }
 
-		public void SetDebug2(string text){
-				ui_debug2.SetText(text);
-		}
+  public void SetDebug2(string text){
+    ui_debug2.SetText(text);
+  }
 
-		public void SetDebug3(string text){
-				ui_debug3.SetText(text);
-		}
+  public void SetDebug3(string text){
+    ui_debug3.SetText(text);
+  }
 
-		public void SetGhostState(Ghost ghost, string state, string power, string power_timer) {
-				ghost_state.SetText(state);
-				ghost_power.SetText(power);
-				ghost_power_timer.SetText(power_timer);
-		}
+  public void SetGhostState(Ghost ghost, string state, string power, string power_timer) {
+    ghost_state.SetText(state);
+    ghost_power.SetText(power);
+    ghost_power_timer.SetText(power_timer);
+  }
 }
