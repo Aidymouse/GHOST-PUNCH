3a0de7eb975755141e03ea3021c94b0f4275561d
Aidan <aidymouse4891@gmail.com>
2026-04-28 23:48:36 +1200

    overhaul UI scripts

---
+++ Assets/Prefabs/UI/UIBar.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour
+{
+		Slider slider;
+
+		public bool useGradient;
+		[Tooltip("Optional - Gradient to use as fill if present. Make sure there's a Fill image child")]
+		public Gradient gradient;
+
+		Image fill;
+
+		public void Start() {
+			slider = this.GetComponent<Slider>();
+			if (useGradient) {
+				fill = this.GetComponentInChildren<Image>();
+			}
+		}
+
+		public void SetMaxValue(float max)
+		{
+				slider.maxValue = max;
+				slider.value = max;
+				if (useGradient) {
+					fill.color = gradient.Evaluate(1f);
+				}
+		}
+
+		public void SetValue(float val)
+		{
+				slider.value = Mathf.Max(val, 0);
+				if (useGradient) {
+					fill.color = gradient.Evaluate(slider.normalizedValue);
+				}
+		}
+}
