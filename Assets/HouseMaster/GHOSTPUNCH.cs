43b7fc60be0c226c1d48a271ef08d7fc1bd451cc
Aidan <aidymouse4891@gmail.com>
2026-06-06 23:59:11 +1200

    test scene to work out transition between shop and house

--- Assets/HouseMaster/GHOSTPUNCH.cs
+++ Assets/HouseMaster/GHOSTPUNCH.cs
@@ -1,15 +1,36 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /** Overall game manager script **/
 public class GHOSTPUNCH : MonoBehaviour
 {
     void Start()
     {
-        
     }
+
+		IEnumerator GenerateHouse() {
+			// TODO make this load the house scene and let it generate
+			AsyncOperation l = SceneManager.LoadSceneAsync("LoadTestGhost", LoadSceneMode.Additive);
+			while (!l.isDone) {
+				yield return null;
+			}
+
+			Debug.Log("Loaded!");
+
+			// I think ghost puncher can just be in this scene and we do some fancy cutscene to control stuff
+		}
 
 
 		public void StartRun () {
+			// Remove the black background from the door
+			// Start the timeline t
+			Debug.Log("Starting Load");
+			StartCoroutine(GenerateHouse());
+		}
+
+		public void EndRun() {
+			// flash of white, teleport to shop scene (which I think will just never unload)
 		}
 
 }
