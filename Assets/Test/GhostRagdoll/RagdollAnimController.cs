9d9dcba7ffe8334a5005776fa080e4ea5bc8b809
Aidan <aidymouse4891@gmail.com>
2026-07-06 22:06:44 +1200

    Ragdoll: put limits on hands + forearms. Modify ragdoll settings
    Turning off interpolation makes the movement work way better, plus
    giving her more weight made her less bouncy

--- Assets/Test/GhostRagdoll/RagdollAnimController.cs
+++ Assets/Test/GhostRagdoll/RagdollAnimController.cs
@@ -5,8 +5,13 @@ using Hairibar.Ragdoll.Animation;
 public class RagdollAnimController : MonoBehaviour {
 	
 	public Animator anim;
+	public RagdollAnimator rag;
+	public Ghost ghost;
+
+	float master_alpha;
 
 	void Start() {
+			master_alpha = rag.MasterAlpha;
 			ChangeAnimation("Idle");
 	}
 
@@ -16,8 +21,22 @@ public class RagdollAnimController : MonoBehaviour {
   }
 
 	public void PlayAnimation(string animation) {
+		Unragdoll();
 		Debug.Log("Playing " + animation);
 		ChangeAnimation(animation);
+	}
+
+	public void Ragdoll() {
+		rag.MasterAlpha = 0;
+	}
+
+	public void Unragdoll() {
+		rag.MasterAlpha = master_alpha;
+	}
+
+	public void PickNewSpot() {
+		ghost.nav_destination = null;
+		ghost.EnterAction(GhostActions.MOVING_ROOM);
 	}
 
 }
