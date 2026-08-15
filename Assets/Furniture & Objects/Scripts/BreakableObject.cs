e32aceec7f32aa270e20ec189bb95bca8f8367f0
Aidan <aidymouse4891@gmail.com>
2026-08-16 11:50:20 +1200

    Object: breakable objects now rely on external material object for particles and sounds

--- Assets/Furniture & Objects/Scripts/BreakableObject.cs
+++ Assets/Furniture & Objects/Scripts/BreakableObject.cs
@@ -7,9 +7,11 @@ public class BreakableObject : MonoBehaviour
 	[Header("Attributes")]
 	[Tooltip("Amount of HP this object has")]
 	public float hp;
-	[Tooltip("If you set weight to anything other than custom, hit attributes will be informed by the ObjectAttributes scriptable object")]
+	[Tooltip("Controls various misc object configs. Probably standard across all objects.")]
 	public ObjectConfig config;
+	[Tooltip("Attributes contains damage and force attributes for an object")]
 	public ObjectAttributes attrs;
+	[Tooltip("Material controls things like particles and sound effects")]
 	public ObjectMaterial material;
 	[Tooltip("When I hit something, they'll use this to figure out what I am. (3) heavy object; (4) light object")]
 	public int hit_class = 4;
@@ -19,36 +21,17 @@ public class BreakableObject : MonoBehaviour
 	public GameObject broken_obj;
 	[Tooltip("Rotation offset to apply to spawned child object (on top of rotation match to parent)")]
 	public Vector3 rotation_offset;
-	[Tooltip("Particles spawned for each child object when breaking into pieces")]
-	public ParticleSystem break_particles;
-
-	[Header("Particles")]
-	[Tooltip("Particles spawned at hit location when hit")]
-	public ParticleSystem hit_particles;
-	[Tooltip("Particles this object spawns for itself when it breaks")]
-	public ParticleSystem break_self_particles;
-
-	[Header("Sound")]
-	[Tooltip("The sound that plays when the object is hit (punched or hit by another object")]
-	public AudioClip hitSoundEffect;
-	[Tooltip("Low bound on pitch adjustment when hit sound plays")]
-	public float pitchLow;
-	[Tooltip("High bound on pitch adjustment when hit sound plays")]
-	public float pitchHigh;
-	[Tooltip("The sound that plays when this object is destroyed")]
-	public AudioClip destroyedSoundEffect;
+	
+	[HideInInspector]
+	public float poise_damage;
+	[HideInInspector]
+	public float ghost_damage;
+	[HideInInspector]
+	public float object_damage;
+	[HideInInspector]
+	public float force;
 
 	AudioSource audio_source;
-
-	[Header("Custom Attributes")]
-	[Tooltip("How much poise damage to deal when hitting the ghost")]
-	public float poise_damage = 0;
-	[Tooltip("How much HP damage to deal when hitting the ghost")]
-	public float ghost_damage = 0;
-	[Tooltip("How much object damage to deal when smashing into another object")]
-	public float object_damage = 0;
-	[Tooltip("How much physics force to apply when hitting an object (this is in addition to normal physics engine force so usually can be 0)")]
-	public float force = 0;
 
 	// If we get changed to the flying object layer, we'll return to this one when we should exit it
 	int? preserved_layer;
@@ -73,22 +56,14 @@ public class BreakableObject : MonoBehaviour
 		ghost_damage = attrs.GHOST_DAMAGE;
 		force = attrs.FORCE;
 
-		if (hitSoundEffect) {
+		if (material && material.hit_sound) {
 			audio_source = GetComponent<AudioSource>();
 			if (!audio_source) {
 				Debug.LogWarning("Breakable object missing an audio source component! Adding one manually...");
 				this.gameObject.AddComponent<AudioSource>();
 			}
-			audio_source.clip = hitSoundEffect;
+			audio_source.clip = material.hit_sound;
 		}
-
-		/*
-		if(gameObject.GetComponent<AudioSource>() != null)
-        {
-			destroyedSound = GetComponent<AudioSource>();
-			destroyedSound.clip = hitSoundEffect;
-		}
-		*/
 		
 	}
 
@@ -190,16 +165,17 @@ public class BreakableObject : MonoBehaviour
 
 
 		//Audio
+		// A breakable object only makes one sound, when it's hit, so we don't need to assign the sound, just play it.
 		if (audio_source) { 
-			audio_source.pitch = (Random.Range(pitchLow, pitchHigh));
+			audio_source.pitch = (Random.Range(material.pitch_low, material.pitch_high));
 			audio_source.Play(); 
 		}
 			
 
 		// spawn particles
 		// TODO: rotation
-		if (hit_particles) {
-					Instantiate(hit_particles, hit_point, new Quaternion());
+		if (material && material.hit_particles) {
+					Instantiate(material.hit_particles, hit_point, new Quaternion());
 			}
 
 			Rigidbody rb = this.GetComponent<Rigidbody>();
@@ -236,8 +212,8 @@ public class BreakableObject : MonoBehaviour
 		Transform initRotation = this.transform;
 		initRotation.Rotate(this.rotation_offset); // Local space ??
 
-		if (break_self_particles) {
-				Instantiate(break_self_particles, hit_point, new Quaternion());
+		if (material && material.break_particles) {
+				Instantiate(material.break_particles, hit_point, new Quaternion());
 		}
 
 		// Spawn broken object
@@ -251,17 +227,21 @@ public class BreakableObject : MonoBehaviour
 			Rigidbody[] rbs = broken.GetComponentsInChildren<Rigidbody>();
 
 			foreach (Rigidbody crb in rbs) {
-				if (break_particles) {
+				// TODO: use child breakable object material spawn particles
+				/*
+				if (material.spawn_) {
 					Instantiate(break_particles, crb.transform.position, new Quaternion());
 				}
+				*/
+
 				crb.isKinematic = false;
 				// TODO: some actual conservation of momentum here?
 				crb.AddForce( (velocity+hit_dir).normalized * (velocity.magnitude + force));
 			}
 		}
 
-		if (destroyedSoundEffect) {
-			SoundEmitter.Create(destroyedSoundEffect);
+		if (material && material.break_sound) {
+			SoundEmitter.Create(material.break_sound);
 		}
 
 		Destroy(this.gameObject);
