a32f8f8383ece394bc635f7ef48f23a3bf69ebd4
Superdank <2002jef@gmail.com>
2026-08-25 16:29:55 +1200

    kickout effect

---
+++ Assets/Shaders/KickoutEffect.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MagicScreenEffectController : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float effectStrength = 0f;
+
+    private static readonly int EffectStrengthID =
+        Shader.PropertyToID("_EffectStrength");
+
+    private void Update()
+    {
+        Shader.SetGlobalFloat(EffectStrengthID, effectStrength);
+    }
+}
