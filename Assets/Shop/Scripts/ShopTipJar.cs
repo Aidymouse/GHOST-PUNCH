261d08bf7377c98c8bde32924da038d6954f84d4
Aidan <aidymouse4891@gmail.com>
2026-09-13 22:27:26 +1200

    Shop: spawn goop on tip jar + try to use particle delay to line it up

--- Assets/Shop/Scripts/ShopTipJar.cs
+++ Assets/Shop/Scripts/ShopTipJar.cs
@@ -3,8 +3,19 @@ using UnityEngine;
 public class ShopTipJar : MonoBehaviour
 {
 	public ParticleSystem particles;
+	public GameObject goop_obj;
+	public Transform goop_spawn;
 
 	public void OnClick() {
 		particles.Play();
+
+		SpawnGoop();
 	}
+
+
+	public void SpawnGoop() {
+		GameObject new_goop = Instantiate(goop_obj, this.transform);
+		new_goop.transform.position = goop_spawn.position;
+	}
+	
 }
